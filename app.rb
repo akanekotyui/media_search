f3c1b8e168dbd7e2f88e530461b77f3f7fbf504f
akanekotyui <a_roid_ba_sky@yahoo.co.jp>
2017-10-27 17:42:00 +0900

    DBが見れるようになった

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@
 require 'sinatra'
 require 'sinatra/reloader' #編集しても再起動せずにアプリが動く
 require 'SQLite3'
-require 'active_record'
+require 'sinatra/activerecord'
 
 
 set :database, adapter: "sqlite3", database: "images.sqlite3"
@@ -11,7 +11,7 @@ require './models/image.rb'
 
 get '/' do
     images = Image.all
-erb :index #ルートにアクセスしたらindex.erbを呼び出す
+erb :index,  locals:{images: images} #ルートにアクセスしたらindex.erbを呼び出す
 end
 
 post '/search.erb' do
--- models/image.rb
+++ models/image.rb
@@ -1,7 +1,5 @@
 #ActiveRecordのモデルを作成する
 #Imageモデルとimagesテーブルを作成
 
-ActiveRecord::Base.establish_connection('sqlite3:///models.db') #image.db
-
 class Image < ActiveRecord::Base
 end
